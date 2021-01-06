7851b1f25f4dcbc42d06ed890fde7ae5745e1296
S F <sebastian.fariasb@usach.cl>
2021-01-06 14:37:39 -0300

    Basic NavBar Ready

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+import { Description, Menu } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const navBar = () =>{
+  const classes = useStyles()
   return(
-    <h1>NavBar</h1>
+    <AppBar position='sticky'>
+      <Toolbar>
+        <IconButton color='inherit' className={classes.menuButton}>
+          <Menu/>{/*Hamburguer Menu Icon*/}
+        </IconButton>
+        <Typography variant='h6' className={classes.title}>
+          Seba Farias
+        </Typography>
+        <Button 
+          variant='text' 
+          color='inherit'
+          startIcon={<Description/>}
+        >
+          CV
+        </Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 
