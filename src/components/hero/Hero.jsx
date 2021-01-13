a1313252aabcbe0f91d102d2cd3f9dc542ced1fc
S F <sebastian.fariasb@usach.cl>
2021-01-13 17:02:21 -0300

    typo corrected

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography'
 const Hero = () => {
   return(
     <Typography align='center' variant='h1' color='secondary'>
-      Este Portafolio me va a dar un peguita
+      Este Portafolio me va a dar una peguita
     </Typography>
   )
 }
