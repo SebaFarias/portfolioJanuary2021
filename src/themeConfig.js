987d9cd2044dbb1df5cf079242d855da3fcbcaa4
S F <sebastian.fariasb@usach.cl>
2021-01-13 17:03:29 -0300

    theme file created

--- src/themeConfig.js
+++ src/themeConfig.js
@@ -1,7 +1,24 @@
 import {createMuiTheme} from '@material-ui/core/styles'
 
-const theme = createMuiTheme({
+const DARK_MODE_KEY = 'dark_mode'
+const LIGHT_THEME = createMuiTheme({
   
 })
-
-export default theme
+const DARK_THEME = createMuiTheme({
+  
+})
+const darkMode = {
+  getTheme: () => {
+      try {
+          return JSON.parse(window.localStorage.getItem(DARK_MODE_KEY)) === true
+      } catch (e) {
+          return false
+      }
+  },
+  setTheme: value => {
+      try {
+          window.localStorage.setItem(DARK_MODE_KEY,JSON.stringify(value === true))
+      } catch (e) {}
+  }
+}
+export { DARK_THEME , LIGHT_THEME , darkMode }
